5d04306dcf34e16c61c65e4463ada464c41fe1ae
QUY TU MINH <tmquy@rd.misa.com.vn>
2023-06-13 11:22:17 +0700

    git commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 (function ($) {
   "use strict";
   //Anti Debug
-  // $(document).keydown(function (event) {
-  //   if (event.keyCode == 123) {
-  //     // Prevent F12
-  //     return false;
-  //   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-  //     // Prevent Ctrl+Shift+I
-  //     return false;
-  //   }
-  // });
-  // $(document).on("contextmenu", function (e) {
-  //   e.preventDefault();
-  // });
+  $(document).keydown(function (event) {
+    if (event.keyCode == 123) {
+      // Prevent F12
+      return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+      // Prevent Ctrl+Shift+I
+      return false;
+    }
+  });
+
+  $(document).ready(() => {
+    let i = Math.floor(Math.random() * (99999 - 10 + 1)) + 10;
+    for (let j = Math.sqrt(i); j <= 99999; j++) {
+      debugger;
+      continue;
+    }
+  });
 
   $("[data-localize]").localize("../constants/language", { language: "es" });
 
